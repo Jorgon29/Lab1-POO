5d85e60b7438090c40cef1f3f4552f9005e09f97
Jorgon29 <00016823@uca.edu.sv>
2024-03-18 17:09:08 -0600

    Agregada multiplicacion y division

--- Calculadora.java
+++ Calculadora.java
@@ -2,9 +2,8 @@ import java.util.*;
 
 public class Calculadora {
 
-    int opcionActual;
-    public static int Sumar(){
-        Scanner sc = new Scanner(System.out);
+    public static float Sumar(){
+        Scanner sc = new Scanner(System.in);
         System.out.println("1er numero");
         float n1 = sc.nextFloat();
         System.out.println("2do numero");
@@ -13,8 +12,8 @@ public class Calculadora {
         return n1 + n2;
     }
 
-    public static int Restar(){
-        Scanner sc = new Scanner(System.out);
+    public static float Restar(){
+        Scanner sc = new Scanner(System.in);
         System.out.println("1er numero");
         float n1 = sc.nextFloat();
         System.out.println("2do numero");
@@ -23,16 +22,42 @@ public class Calculadora {
         return n1 - n2;
     }
 
-    
+    public static float Multiplicar(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1er numero");
+        float n1 = sc.nextFloat();
+        System.out.println("2do numero");
+        float n2 = sc.nextFloat();
+        System.out.println(n1 + " * " + n2 + " = " + (n1*n2));
+        return n1 - n2;
+    }
+
+    public static float Dividir(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1er numero");
+        float n1 = sc.nextFloat();
+        System.out.println("2do numero");
+        float n2 = sc.nextFloat();
+        if (n2 != 0){
+            System.out.println(n1 + " / " + n2 + " = " + (n1/n2));
+            return n1 - n2;
+        }
+        return 0;
+
+    }
+
+
     public static void main(String[] args){
-        opcionActual = 0;
+        int opcionActual = 0;
         do {
+            Scanner scan = new Scanner(System.in);
             System.out.println("Elije una opcion");
             System.out.println("0 - Sumar");
             System.out.println("1 - Restar");
             System.out.println("2 - Multiplicar");
             System.out.println("3 - Dividr");
             System.out.println("4 - Salir");
+            opcionActual = scan.nextInt();
             switch (opcionActual){
                 case 0:
                     Sumar();
@@ -45,10 +70,12 @@ public class Calculadora {
                     break;
                 case 3:
                     Dividir();
+                case 4:
+                    break;
                 default:
-                    System.out.println("Opcion invalidad");
+                    System.out.println("Opcion invalida");
 
             }
-        } while (opcion != 4);
+        } while (opcionActual != 4);
     }
 }
